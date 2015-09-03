61fecb19cf2dc96dc837d36cc5dc103cefd41d16
Maslab eeePC <blacksheepmails@gmail.com>
2015-09-03 02:35:43 +0100

    added map2 add sub and hadamard

--- src/Matrix/Extra.elm
+++ src/Matrix/Extra.elm
@@ -1,22 +1,27 @@
 module Matrix.Extra where
 
 import Matrix exposing (Matrix)
+import Array exposing (fromList, toList)
+import List exposing (map2)
+
+map2 f a b = if a.size == b.size 
+                then Just { a | data <- Array.fromList <| List.map2 f (Array.toList a.data) (Array.toList b.data) }
+                else Nothing
 
 {-|
 add two matricies together element by element and return the result
 -}
-add : Matrix a -> Matrix a -> Maybe (Matrix a)
-add a b = Just a
-
+add : Matrix number -> Matrix number -> Maybe (Matrix number)
+add a b = map2 (+) a b
 
 {-|
 subtract two matricies together element by element and return the result
 -}
-subtract : Matrix a -> Matrix a -> Maybe (Matrix a)
-subtract a b = Just a
+subtract : Matrix number -> Matrix number -> Maybe (Matrix number)
+subtract a b = map2 (-) a b
 
 {-|
 take the product of every corresponding element in two matricies and return the result
 -}
-hadamard : Matrix a -> Matrix a -> Maybe (Matrix a)
-hadamard a b = Just a
+hadamard : Matrix number -> Matrix number -> Maybe (Matrix number)
+hadamard a b = map2 (*) a b

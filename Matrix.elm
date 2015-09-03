b41f708e7ad89bbbfd985e7a7fe01822cfedcce5
eeue56 <enalicho@gmail.com>
2015-09-03 01:18:58 +0100

    add docs

--- Matrix.elm
+++ Matrix.elm
@@ -3,15 +3,26 @@ module Matrix where
 import Array exposing (Array)
 import List
 
+{-|
+Matrix a has a given size, and data contained within
+-}
 type alias Matrix a = {
   size: (Int, Int),
   data : Array (a) }
 
+{-| 
+Create a matrix of a given size x y with a default value of v
+-}
 repeat : Int -> Int -> a -> Matrix a 
 repeat x y v = {
   size = (x, y),
   data = Array.repeat (x * y) v }
   
+{-|
+Create a matrix from a list of lists.
+If the lists within the list are not consistently sized, return Nothing
+Otherwise return a matrix with the size as the size of the outer and nested lists
+-}
 fromList : List (List a) -> Maybe (Matrix a)
 fromList list =
   let
@@ -23,6 +34,10 @@ fromList list =
     else Just { size = (size', nestedSize), data = Array.fromList <| List.concat list }
     
 
+{-|
+Get a value from a given x y and return Just v if it exists
+Otherwise Nothing
+-}
 get : Int -> Int -> Matrix a  -> Maybe a
 get i j matrix = 
   let
@@ -30,6 +45,10 @@ get i j matrix =
   in
     Array.get pos matrix.data
 
+{-|
+Set a value at a given i, j in the matrix and return the new matrix
+If the i, j is out of bounds then return the unmodified matrix
+-}
 set : Int -> Int -> a -> Matrix a -> Matrix a
 set i j v matrix = 
   let

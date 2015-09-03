ce2748dbe798562524797136d6d8bb979312767e
eeue56 <enalicho@gmail.com>
2015-09-03 02:25:37 +0100

    update docs

--- src/Matrix.elm
+++ src/Matrix.elm
@@ -20,14 +20,14 @@ import Array exposing (Array)
 import List
 
 {-|
-Matrix a has a given size, and data contained within
+  Matrix a has a given size, and data contained within
 -}
 type alias Matrix a = {
   size: (Int, Int),
   data : Array (a) }
 
 {-| 
-Create a matrix of a given size x y with a default value of v
+  Create a matrix of a given size x y with a default value of v
 -}
 repeat : Int -> Int -> a -> Matrix a 
 repeat x y v = {
@@ -35,9 +35,9 @@ repeat x y v = {
   data = Array.repeat (x * y) v }  
   
 {-|
-Create a matrix from a list of lists.
-If the lists within the list are not consistently sized, return Nothing
-Otherwise return a matrix with the size as the size of the outer and nested lists
+  Create a matrix from a list of lists.
+  If the lists within the list are not consistently sized, return Nothing
+  Otherwise return a matrix with the size as the size of the outer and nested lists
 -}
 fromList : List (List a) -> Maybe (Matrix a)
 fromList list =
@@ -50,8 +50,8 @@ fromList list =
     else Just { size = (size', nestedSize), data = Array.fromList <| List.concat list }
 
 {-|
-Get a value from a given x y and return Just v if it exists
-Otherwise Nothing
+  Get a value from a given x y and return Just v if it exists
+  Otherwise Nothing
 -}
 get : Int -> Int -> Matrix a  -> Maybe a
 get i j matrix = 
@@ -61,8 +61,8 @@ get i j matrix =
     Array.get pos matrix.data
 
 {-|
-Set a value at a given i, j in the matrix and return the new matrix
-If the i, j is out of bounds then return the unmodified matrix
+  Set a value at a given i, j in the matrix and return the new matrix
+  If the i, j is out of bounds then return the unmodified matrix
 -}
 set : Int -> Int -> a -> Matrix a -> Matrix a
 set i j v matrix = 
@@ -72,8 +72,8 @@ set i j v matrix =
     { matrix | data <- Array.set pos v matrix.data }
 
 {-|
-Update an element at x, y with the given update function
-If out of bounds, return the matrix unchanged
+  Update an element at x, y with the given update function
+  If out of bounds, return the matrix unchanged
 -}
 update : Int -> Int -> (a -> a) -> Matrix a -> Matrix a
 update x y f matrix =
@@ -82,14 +82,14 @@ update x y f matrix =
     Just v -> set x y (f v) matrix
 
 {-| 
-Apply a function of every element in the matrix
+  Apply a function of every element in the matrix
 -}
 map : (a -> b) -> Matrix a -> Matrix b
 map f matrix = 
   { matrix | data <- Array.map f matrix.data }
 
 {-| 
-Apply a function, taking the (x, y) of every element in the matrix
+  Apply a function, taking the (x, y) of every element in the matrix
 -}
 indexedMap : (Int -> Int -> a -> b) -> Matrix a -> Matrix b
 indexedMap f matrix = 
@@ -104,7 +104,7 @@ indexedMap f matrix =
     { matrix | data <- Array.indexedMap f' matrix.data }
 
 {-| 
-Keep only elements that return True when passed to the given function f
+  Keep only elements that return True when passed to the given function f
 -}
 filter : (a -> Bool) -> Matrix a -> Array a
 filter f matrix = 

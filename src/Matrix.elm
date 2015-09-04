c26d06385cf3c5c025158be77cc6da623bd9c217
eeue56 <enalicho@gmail.com>
2015-09-04 02:56:55 +0100

    bug fixes

--- src/Matrix.elm
+++ src/Matrix.elm
@@ -176,7 +176,7 @@ indexedMap f matrix =
       in 
         f x y v
   in
-    { matrix | data <- Array.fromList <| List.indexedMap f' matrix.data }
+    { matrix | data <- Array.fromList <| List.indexedMap f' <| Array.toList matrix.data }
 
 {-| 
   Keep only elements that return `True` when passed to the given function f

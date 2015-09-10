80ccf708511ed502ebfcf7527f97e09d532b7e57
eeue56 <enalicho@gmail.com>
2015-09-10 23:18:51 +0100

    find a way of doing neighbours

--- src/Matrix/Extra.elm
+++ src/Matrix/Extra.elm
@@ -1,7 +1,8 @@
 module Matrix.Extra (
     add, subtract, 
     hadamard, (.*),
-    power, (.^)) where
+    power, (.^),
+    neighbours) where
 {-| Extra methods for Matricies
 
 # Element-wise computation
@@ -10,11 +11,42 @@ module Matrix.Extra (
 # Syntax aliases
 @docs (.*), (.^) 
 
+# Interacting with other cells
+@docs neighbours
+
 -}
-import Matrix exposing (Matrix, map2)
+import Matrix exposing (Matrix, map2, get)
 import Array exposing (fromList, toList)
 
+{-|
+Get the neighbours of a point (x, y) in the matrix
+If on edge, then no wrapping happens - they are excluded
+-}
+neighbours : Int -> Int -> Matrix a  -> List a
+neighbours x y matrix = 
+  let 
+    grab di dj =
+      Matrix.get (x + di) (y + dj) matrix 
+  in
+    List.map (\x -> case x of Just v -> v)
+    <| List.filter (\x -> case x of
+        Just y -> True
+        Nothing -> False)
+    <| [
+        -- left
+        grab -1 -1, 
+        grab -1 0, 
+        grab -1 1, 
 
+        -- middle, exclude center
+        grab 0 -1, 
+        grab 0 1, 
+
+        -- right
+        grab 1 -1, 
+        grab 1 0,
+        grab 1 1
+      ]
 
 {-|
 add two matricies together element by element and return the result

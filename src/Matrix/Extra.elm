2e55d389146a42ca4e1b2028303777ac0753d415
eeue56 <enalicho@gmail.com>
2015-09-10 23:28:33 +0100

    add more neighbour helpers

--- src/Matrix/Extra.elm
+++ src/Matrix/Extra.elm
@@ -2,7 +2,7 @@ module Matrix.Extra (
     add, subtract, 
     hadamard, (.*),
     power, (.^),
-    neighbours) where
+    neighbours, diagonals, indexedNeighbours, neighboursFour) where
 {-| Extra methods for Matricies
 
 # Element-wise computation
@@ -12,7 +12,7 @@ module Matrix.Extra (
 @docs (.*), (.^) 
 
 # Interacting with other cells
-@docs neighbours
+@docs neighbours, indexedNeighbours, diagonals, neighboursFour
 
 -}
 import Matrix exposing (Matrix, map2, get)
@@ -46,6 +46,89 @@ neighbours x y matrix =
         grab 1 -1, 
         grab 1 0,
         grab 1 1
+      ]
+
+{-|
+Get the neighbours of a point (x, y) in the matrix
+If on edge, then no wrapping happens - they are excluded
+-}
+indexedNeighbours : Int -> Int -> Matrix a  -> List ((Int, Int), a)
+indexedNeighbours x y matrix = 
+  let 
+    grab di dj =
+      let 
+        nx = di + x
+        ny = dj + y
+      in 
+        ((nx, ny), Matrix.get nx ny matrix)
+  in
+    List.map (\(pos, x) -> case x of Just v -> (pos, v))
+    <| List.filter (\(_, x) -> case x of
+        Just y -> True
+        Nothing -> False)
+    <| [
+        -- left
+        grab -1 -1, 
+        grab -1 0, 
+        grab -1 1, 
+
+        -- middle, exclude center
+        grab 0 -1, 
+        grab 0 1, 
+
+        -- right
+        grab 1 -1, 
+        grab 1 0,
+        grab 1 1
+      ]
+
+{-|
+Get the diagonal-neighbours of a point (x, y) in the matrix
+If on edge, then no wrapping happens - they are excluded
+-}
+diagonals : Int -> Int -> Matrix a  -> List a
+diagonals x y matrix = 
+  let 
+    grab di dj =
+      Matrix.get (x + di) (y + dj) matrix 
+  in
+    List.map (\x -> case x of Just v -> v)
+    <| List.filter (\x -> case x of
+        Just y -> True
+        Nothing -> False)
+    <| [
+        -- left side
+        grab -1 -1, 
+        grab -1 1, 
+        -- right side
+        grab 1 -1, 
+        grab 1 1
+      ]
+
+{-|
+Get the non-diagonal neighbours of a point (x, y) in the matrix
+If on edge, then no wrapping happens - they are excluded
+-}
+neighboursFour : Int -> Int -> Matrix a  -> List a
+neighboursFour x y matrix = 
+  let 
+    grab di dj =
+      Matrix.get (x + di) (y + dj) matrix 
+  in
+    List.map (\x -> case x of Just v -> v)
+    <| List.filter (\x -> case x of
+        Just y -> True
+        Nothing -> False)
+    <| [
+        -- left
+        grab -1 0, 
+
+        -- middle, exclude center
+        grab 0 -1, 
+        grab 0 1, 
+
+        -- right
+        grab 1 0
       ]
 
 {-|

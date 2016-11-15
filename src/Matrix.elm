f9dff7793f73dba51c25bbbf2407da1418df6ac9
noah <noah@noredink.com>
2016-11-15 00:41:15 +0000

    0.18

--- src/Matrix.elm
+++ src/Matrix.elm
@@ -1,10 +1,25 @@
-module Matrix exposing (Matrix,
-  height, width,
-  repeat, fromList,
-  get, getRow, getColumn,
-  set, update, concatVertical, concatHorizontal,
-  toIndexedArray, empty,
-  map, map2, indexedMap, filter)
+module Matrix
+    exposing
+        ( Matrix
+        , height
+        , width
+        , repeat
+        , fromList
+        , get
+        , getRow
+        , getColumn
+        , set
+        , update
+        , concatVertical
+        , concatHorizontal
+        , toIndexedArray
+        , empty
+        , map
+        , map2
+        , indexedMap
+        , filter
+        )
+
 {-|
 A matrix implemention for Elm.
 Internally it uses a flat array for speed reasons.
@@ -40,33 +55,46 @@ Internally it uses a flat array for speed reasons.
 import Array exposing (Array)
 import List
 
+
 {-|
   Matrix a has a given size, and data contained within
 -}
-type alias Matrix a = {
-  size: (Int, Int),
-  data : Array (a) }
+type alias Matrix a =
+    { size : ( Int, Int )
+    , data : Array a
+    }
 
 
-{-| Create an empty matrix -}
+{-| Create an empty matrix
+-}
 empty : Matrix a
-empty = { size = (0, 0), data = Array.empty}
+empty =
+    { size = ( 0, 0 ), data = Array.empty }
 
-{-| Width of a given matrix -}
+
+{-| Width of a given matrix
+-}
 width : Matrix a -> Int
-width matrix = fst matrix.size
+width matrix =
+    Tuple.first matrix.size
 
-{-| Height of a given matrix -}
+
+{-| Height of a given matrix
+-}
 height : Matrix a -> Int
-height matrix = snd matrix.size
+height matrix =
+    Tuple.second matrix.size
+
 
 {-|
   Create a matrix of a given size `x y` with a default value of `v`
 -}
 repeat : Int -> Int -> a -> Matrix a
-repeat x y v = {
-  size = (x, y),
-  data = Array.repeat (x * y) v }
+repeat x y v =
+    { size = ( x, y )
+    , data = Array.repeat (x * y) v
+    }
+
 
 {-|
   Create a matrix from a list of lists.
@@ -75,19 +103,30 @@ repeat x y v = {
 -}
 fromList : List (List a) -> Maybe (Matrix a)
 fromList list =
-  let
-    -- the number of elements in the top level list is taken as height
-    height = List.length list
-    -- the number of elements in the first element is taken as the width
-    width = List.length <|
-      case List.head list of
-        Just x -> x
-        Nothing -> []
-    -- ensure that all "rows" are the same size
-    allSame = List.isEmpty <| List.filter (\x -> List.length x /= width) list
-  in
-    if not allSame then Nothing
-    else Just { size = (width, height), data = Array.fromList <| List.concat list }
+    let
+        -- the number of elements in the top level list is taken as height
+        height =
+            List.length list
+
+        -- the number of elements in the first element is taken as the width
+        width =
+            List.length <|
+                case List.head list of
+                    Just x ->
+                        x
+
+                    Nothing ->
+                        []
+
+        -- ensure that all "rows" are the same size
+        allSame =
+            List.isEmpty <| List.filter (\x -> List.length x /= width) list
+    in
+        if not allSame then
+            Nothing
+        else
+            Just { size = ( width, height ), data = Array.fromList <| List.concat list }
+
 
 {-|
   Get a value from a given `x y` and return `Just v` if it exists
@@ -95,65 +134,112 @@ fromList list =
 -}
 get : Int -> Int -> Matrix a -> Maybe a
 get i j matrix =
-  let
-    pos = (j * (width matrix)) + i
-  in
-    if (i < width matrix && i > -1) && (j < height matrix && j > -1) then Array.get pos matrix.data
-    else Nothing
+    let
+        pos =
+            (j * (width matrix)) + i
+    in
+        if (i < width matrix && i > -1) && (j < height matrix && j > -1) then
+            Array.get pos matrix.data
+        else
+            Nothing
+
 
 {-| Get a row at a given j
 -}
 getRow : Int -> Matrix a -> Maybe (Array a)
 getRow j matrix =
-  let
-    start = (j * (width matrix) )
-    end = start + width matrix
-  in
-    if end > ((width matrix) * (height matrix)) then Nothing
-    else Just <| Array.slice start end matrix.data
+    let
+        start =
+            (j * (width matrix))
+
+        end =
+            start + width matrix
+    in
+        if end > ((width matrix) * (height matrix)) then
+            Nothing
+        else
+            Just <| Array.slice start end matrix.data
+
 
 {-| Get a row at a given i
 -}
 getColumn : Int -> Matrix a -> Maybe (Array a)
 getColumn i matrix =
-  let
-    width = fst matrix.size
-    height = snd matrix.size
-    indices = List.map (\x -> x*width + i) [0..(height-1)]
-  in
-    if i >= width
-      then Nothing
-      else Just <| Array.fromList <| List.foldl (\index ls ->
-        case Array.get index matrix.data of
-          Just v -> ls ++ [v]
-          Nothing -> ls
-      ) [] indices
+    let
+        width =
+            Tuple.first matrix.size
 
-{-| Append a matrix to another matrix horizontally and return the result. Return Nothing if the heights don't match -}
+        height =
+            Tuple.second matrix.size
+
+        indices =
+            List.map (\x -> x * width + i) (List.range 0 (height - 1))
+    in
+        if i >= width then
+            Nothing
+        else
+            Just <|
+                Array.fromList <|
+                    List.foldl
+                        (\index ls ->
+                            case Array.get index matrix.data of
+                                Just v ->
+                                    ls ++ [ v ]
+
+                                Nothing ->
+                                    ls
+                        )
+                        []
+                        indices
+
+
+{-| Append a matrix to another matrix horizontally and return the result. Return Nothing if the heights don't match
+-}
 concatHorizontal : Matrix a -> Matrix a -> Maybe (Matrix a)
 concatHorizontal a b =
-  let
-    finalWidth = fst a.size + fst b.size
-    insert i xs array = Array.append
-                          (Array.append (Array.slice 0 i array) xs)
-                          (Array.slice i (Array.length array) array)
-  in
-    if snd a.size /= snd b.size then Nothing
-    else Just <| { a | size = (finalWidth, snd a.size)
-                     , data = List.foldl
-                                (\(i,xs) acc -> insert (i*finalWidth) xs acc)
-                                b.data
-                                <| List.foldl (\i ls ->
-                                  case getRow i a of
-                                    Just v -> ls ++ [(i,v)]
-                                    Nothing -> ls) [] [0..(snd a.size)-1]
-                 }
+    let
+        finalWidth =
+            Tuple.first a.size + Tuple.first b.size
 
-{-| Append a matrix to another matrix vertically and return the result. Return Nothing if the widths don't match -}
+        insert i xs array =
+            Array.append
+                (Array.append (Array.slice 0 i array) xs)
+                (Array.slice i (Array.length array) array)
+    in
+        if Tuple.second a.size /= Tuple.second b.size then
+            Nothing
+        else
+            Just <|
+                { a
+                    | size = ( finalWidth, Tuple.second a.size )
+                    , data =
+                        List.foldl
+                            (\( i, xs ) acc -> insert (i * finalWidth) xs acc)
+                            b.data
+                        <|
+                            List.foldl
+                                (\i ls ->
+                                    case getRow i a of
+                                        Just v ->
+                                            ls ++ [ ( i, v ) ]
+
+                                        Nothing ->
+                                            ls
+                                )
+                                []
+                                (List.range 0 ((Tuple.second a.size) - 1))
+                }
+
+
+{-| Append a matrix to another matrix vertically and return the result. Return Nothing if the widths don't match
+-}
 concatVertical : Matrix a -> Matrix a -> Maybe (Matrix a)
 concatVertical a b =
-  if fst a.size /= fst b.size then Nothing
-  else Just <| { a | size = (fst a.size, snd a.size + snd b.size), data = Array.append a.data b.data}
+    if Tuple.first a.size /= Tuple.first b.size then
+        Nothing
+    else
+        Just <| { a | size = ( Tuple.first a.size, Tuple.second a.size + Tuple.second b.size ), data = Array.append a.data b.data }
+
 
 {-|
   Set a value at a given `i, j` in the matrix and return the new matrix
@@ -161,13 +247,15 @@ concatVertical a b =
 -}
 set : Int -> Int -> a -> Matrix a -> Matrix a
 set i j v matrix =
-  let
-    pos = (j * fst matrix.size) + i
-  in
-    if (i < width matrix && i > -1) && (j < height matrix && j > -1) then
-      { matrix | data = Array.set pos v matrix.data }
-    else
-      matrix
+    let
+        pos =
+            (j * Tuple.first matrix.size) + i
+    in
+        if (i < width matrix && i > -1) && (j < height matrix && j > -1) then
+            { matrix | data = Array.set pos v matrix.data }
+        else
+            matrix
+
 
 {-|
   Update an element at `x, y` with the given update function
@@ -175,48 +263,61 @@ set i j v matrix =
 -}
 update : Int -> Int -> (a -> a) -> Matrix a -> Matrix a
 update x y f matrix =
-  case get x y matrix of
-    Nothing -> matrix
-    Just v -> set x y (f v) matrix
+    case get x y matrix of
+        Nothing ->
+            matrix
+
+        Just v ->
+            set x y (f v) matrix
+
 
 {-|
   Apply a function of every element in the matrix
 -}
 map : (a -> b) -> Matrix a -> Matrix b
 map f matrix =
-  { matrix | data = Array.map f matrix.data }
+    { matrix | data = Array.map f matrix.data }
 
 
 {-| Apply a function to two matricies at once
 -}
 map2 : (a -> b -> c) -> Matrix a -> Matrix b -> Maybe (Matrix c)
-map2 f a b = if a.size == b.size
-                then Just { a | data = Array.fromList <| List.map2 f (Array.toList a.data) (Array.toList b.data) }
-                else Nothing
+map2 f a b =
+    if a.size == b.size then
+        Just { a | data = Array.fromList <| List.map2 f (Array.toList a.data) (Array.toList b.data) }
+    else
+        Nothing
+
 
 {-|
   Apply a function, taking the `x, y` of every element in the matrix
 -}
 indexedMap : (Int -> Int -> a -> b) -> Matrix a -> Matrix b
 indexedMap f matrix =
-  let
-    f' i v =
-      let
-        x = i % (width matrix)
-        y = i // (width matrix)
-      in
-        f x y v
-  in
-    { matrix | data = Array.fromList <| List.indexedMap f' <| Array.toList matrix.data }
+    let
+        f_ i v =
+            let
+                x =
+                    i % (width matrix)
+
+                y =
+                    i // (width matrix)
+            in
+                f x y v
+    in
+        { matrix | data = Array.fromList <| List.indexedMap f_ <| Array.toList matrix.data }
+
 
 {-|
   Keep only elements that return `True` when passed to the given function f
 -}
 filter : (a -> Bool) -> Matrix a -> Array a
 filter f matrix =
-  Array.filter f matrix.data
+    Array.filter f matrix.data
+
 
 {-| Convert a matrix to an indexed array
 -}
-toIndexedArray : Matrix a -> Array ((Int, Int), a)
-toIndexedArray matrix = (indexedMap (\x y v -> ((x, y), v)) matrix).data
+toIndexedArray : Matrix a -> Array ( ( Int, Int ), a )
+toIndexedArray matrix =
+    (indexedMap (\x y v -> ( ( x, y ), v )) matrix).data
--- src/Matrix/Extra.elm
+++ src/Matrix/Extra.elm
@@ -1,9 +1,18 @@
-module Matrix.Extra exposing (
-    add, subtract,
-    hadamard, (.*),
-    power, (.^),
-    prettyPrint,
-    neighbours, diagonals, indexedNeighbours, neighboursFour)
+module Matrix.Extra
+    exposing
+        ( add
+        , subtract
+        , hadamard
+        , (.*)
+        , power
+        , (.^)
+        , prettyPrint
+        , neighbours
+        , diagonals
+        , indexedNeighbours
+        , neighboursFour
+        )
+
 {-| Extra methods for Matricies
 
 # Element-wise computation
@@ -18,193 +27,251 @@ module Matrix.Extra exposing (
 @docs prettyPrint
 
 -}
+
 import Matrix exposing (Matrix, map2, get, getRow, height, width)
 import Array exposing (fromList, toList)
 import Html exposing (Html, table, tr, td)
 import Html.Attributes exposing (style)
 import Element exposing (show, toHtml)
 
+
 -- Helper function for unpacking lists
+
+
 unpackMaybeList : (a -> Maybe b) -> List a -> List b
 unpackMaybeList fn ls =
-  List.foldl (\item ls ->
-    case fn item of
-      Just it -> ls ++ [it]
-      Nothing -> ls
-    ) [] ls
+    List.foldl
+        (\item ls ->
+            case fn item of
+                Just it ->
+                    ls ++ [ it ]
+
+                Nothing ->
+                    ls
+        )
+        []
+        ls
+
 
 {-|
 Print out a matrix into a table
 -}
 prettyPrint : Matrix a -> Html msg
 prettyPrint matrix =
-  let
-    printXIndex = tr [] (td [style [("background-color", "black") ]] [] :: List.map printXCell [0..(width matrix)-1] )
-    printXCell cell =
-        td [style [ ("border", "1px solid black" ), ("background-color", "#A8A8F5") ] ]
-          <| [toHtml <| show cell]
-    printCell cell =
-      td [style [ ("border", "1px solid black" ) ] ]
-        <| [toHtml <| show cell]
-    printRow i row =
-        tr []
-          <| (printXCell i) :: (Array.toList <| Array.map printCell row)
-  in
-    table []
-      <| printXIndex
-      :: (List.indexedMap printRow
-      <| unpackMaybeList (\i -> getRow i matrix) [0..(height matrix)-1])
+    let
+        printXIndex =
+            tr [] (td [ style [ ( "background-color", "black" ) ] ] [] :: List.map printXCell (List.range 0 ((width matrix) - 1)))
+
+        printXCell cell =
+            td [ style [ ( "border", "1px solid black" ), ( "background-color", "#A8A8F5" ) ] ] <|
+                [ toHtml <| show cell ]
+
+        printCell cell =
+            td [ style [ ( "border", "1px solid black" ) ] ] <|
+                [ toHtml <| show cell ]
+
+        printRow i row =
+            tr [] <|
+                (printXCell i)
+                    :: (Array.toList <| Array.map printCell row)
+    in
+        table [] <|
+            printXIndex
+                :: (List.indexedMap printRow <|
+                        unpackMaybeList (\i -> getRow i matrix) (List.range 0 ((height matrix) - 1))
+                   )
 
 
 {-|
 Get the neighbours of a point (x, y) in the matrix
 If on edge, then no wrapping happens - they are excluded
 -}
-neighbours : Int -> Int -> Matrix a  -> List a
+neighbours : Int -> Int -> Matrix a -> List a
 neighbours x y matrix =
-  let
-    grab di dj =
-      Matrix.get (x + di) (y + dj) matrix
-  in
-    unpackMaybeList identity
-    <| List.filter (\x -> case x of
-        Just y -> True
-        Nothing -> False)
-    <| [
-        -- left
-        grab -1 -1,
-        grab -1 0,
-        grab -1 1,
+    let
+        grab di dj =
+            Matrix.get (x + di) (y + dj) matrix
+    in
+        unpackMaybeList identity <|
+            List.filter
+                (\x ->
+                    case x of
+                        Just y ->
+                            True
 
-        -- middle, exclude center
-        grab 0 -1,
-        grab 0 1,
+                        Nothing ->
+                            False
+                )
+            <|
+                [ -- left
+                  grab -1 -1
+                , grab -1 0
+                , grab -1 1
+                , -- middle, exclude center
+                  grab 0 -1
+                , grab 0 1
+                , -- right
+                  grab 1 -1
+                , grab 1 0
+                , grab 1 1
+                ]
 
-        -- right
-        grab 1 -1,
-        grab 1 0,
-        grab 1 1
-      ]
 
 {-|
 Get the neighbours of a point (x, y) in the matrix
 If on edge, then no wrapping happens - they are excluded
 -}
-indexedNeighbours : Int -> Int -> Matrix a  -> List ((Int, Int), a)
+indexedNeighbours : Int -> Int -> Matrix a -> List ( ( Int, Int ), a )
 indexedNeighbours x y matrix =
-  let
-    grab di dj =
-      let
-        nx = di + x
-        ny = dj + y
-      in
-        ((nx, ny), Matrix.get nx ny matrix)
-  in
-    unpackMaybeList (\(pos, x) ->
-      case x of
-        Just v -> Just (pos, v)
-        Nothing -> Nothing
-    )
-    <| List.filter (\(_, x) -> case x of
-        Just y -> True
-        Nothing -> False)
-    <| [
-        -- left
-        grab -1 -1,
-        grab -1 0,
-        grab -1 1,
+    let
+        grab di dj =
+            let
+                nx =
+                    di + x
 
-        -- middle, exclude center
-        grab 0 -1,
-        grab 0 1,
+                ny =
+                    dj + y
+            in
+                ( ( nx, ny ), Matrix.get nx ny matrix )
+    in
+        unpackMaybeList
+            (\( pos, x ) ->
+                case x of
+                    Just v ->
+                        Just ( pos, v )
 
-        -- right
-        grab 1 -1,
-        grab 1 0,
-        grab 1 1
-      ]
+                    Nothing ->
+                        Nothing
+            )
+        <|
+            List.filter
+                (\( _, x ) ->
+                    case x of
+                        Just y ->
+                            True
+
+                        Nothing ->
+                            False
+                )
+            <|
+                [ -- left
+                  grab -1 -1
+                , grab -1 0
+                , grab -1 1
+                , -- middle, exclude center
+                  grab 0 -1
+                , grab 0 1
+                , -- right
+                  grab 1 -1
+                , grab 1 0
+                , grab 1 1
+                ]
+
 
 {-|
 Get the diagonal-neighbours of a point (x, y) in the matrix
 If on edge, then no wrapping happens - they are excluded
 -}
-diagonals : Int -> Int -> Matrix a  -> List a
+diagonals : Int -> Int -> Matrix a -> List a
 diagonals x y matrix =
-  let
-    grab di dj =
-      Matrix.get (x + di) (y + dj) matrix
-  in
-    unpackMaybeList identity
-    <| List.filter (\x -> case x of
-        Just y -> True
-        Nothing -> False)
-    <| [
-        -- left side
-        grab -1 -1,
-        grab -1 1,
-        -- right side
-        grab 1 -1,
-        grab 1 1
-      ]
+    let
+        grab di dj =
+            Matrix.get (x + di) (y + dj) matrix
+    in
+        unpackMaybeList identity <|
+            List.filter
+                (\x ->
+                    case x of
+                        Just y ->
+                            True
+
+                        Nothing ->
+                            False
+                )
+            <|
+                [ -- left side
+                  grab -1 -1
+                , grab -1 1
+                , -- right side
+                  grab 1 -1
+                , grab 1 1
+                ]
+
 
 {-|
 Get the non-diagonal neighbours of a point (x, y) in the matrix
 If on edge, then no wrapping happens - they are excluded
 -}
-neighboursFour : Int -> Int -> Matrix a  -> List a
+neighboursFour : Int -> Int -> Matrix a -> List a
 neighboursFour x y matrix =
-  let
-    grab di dj =
-      Matrix.get (x + di) (y + dj) matrix
-  in
-    unpackMaybeList identity
-    <| List.filter (\x -> case x of
-        Just y -> True
-        Nothing -> False)
-    <| [
-        -- left
-        grab -1 0,
+    let
+        grab di dj =
+            Matrix.get (x + di) (y + dj) matrix
+    in
+        unpackMaybeList identity <|
+            List.filter
+                (\x ->
+                    case x of
+                        Just y ->
+                            True
 
-        -- middle, exclude center
-        grab 0 -1,
-        grab 0 1,
+                        Nothing ->
+                            False
+                )
+            <|
+                [ -- left
+                  grab -1 0
+                , -- middle, exclude center
+                  grab 0 -1
+                , grab 0 1
+                , -- right
+                  grab 1 0
+                ]
 
-        -- right
-        grab 1 0
-      ]
 
 {-|
 add two matricies together element by element and return the result
 -}
 add : Matrix number -> Matrix number -> Maybe (Matrix number)
-add a b = map2 (+) a b
+add a b =
+    map2 (+) a b
+
 
 {-|
 subtract two matricies together element by element and return the result
 -}
 subtract : Matrix number -> Matrix number -> Maybe (Matrix number)
-subtract a b = map2 (-) a b
+subtract a b =
+    map2 (-) a b
+
 
 {-|
 take the product of every corresponding element in two matricies and return the result
 -}
 hadamard : Matrix number -> Matrix number -> Maybe (Matrix number)
-hadamard a b = map2 (*) a b
+hadamard a b =
+    map2 (*) a b
+
 
 {-|
 element-wise power of elements
 -}
 power : Matrix number -> Matrix number -> Maybe (Matrix number)
-power a b = map2 (^) a b
+power a b =
+    map2 (^) a b
+
 
 {-|
 element wise multiplication
 -}
 (.*) : Matrix number -> Matrix number -> Maybe (Matrix number)
-(.*) = hadamard
+(.*) =
+    hadamard
+
 
 {-|
 element wise power
 -}
 (.^) : Matrix number -> Matrix number -> Maybe (Matrix number)
-(.^) = power
+(.^) =
+    power
